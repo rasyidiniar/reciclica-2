34715a5a8fbdc7cdc6a42804a9857891d3e0f348
rasyidiniar <rasyidiniar21@gmail.com>
2024-09-01 23:22:24 +0700

    Initial commit

---
+++ reciclica-app/src/app/pages/login/login.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoginPageRoutingModule } from './login-routing.module';
+
+import { LoginPage } from './login.page';
+import { ErrorMessageComponent } from 'src/app/components/error-message/error-message.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoginPageRoutingModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    LoginPage,
+    ErrorMessageComponent
+  ]
+})
+export class LoginPageModule {}
